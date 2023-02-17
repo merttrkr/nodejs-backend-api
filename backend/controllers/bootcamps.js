46a82e4bc0cf9cbd05e43a604e6bbebe7e311337
merttrkr <mertturker99@gmail.com>
2023-02-17 13:20:45 +0300

    create database connection

--- backend/controllers/bootcamps.js
+++ backend/controllers/bootcamps.js
@@ -1,35 +1,40 @@
-
 //  @desc       get all bootcamps
 //  @route      GET /api/v1/bootcamps
 //  @access     Public
-exports.getBootcamps = (req,res,next)=> {
-    res.status(200).json({ success: true, msg: 'show all bootcamps', hello: req.hello});
-}
+exports.getBootcamps = (req, res, next) => {
+  res
+    .status(200)
+    .json({ success: true, msg: 'show all bootcamps', hello: req.hello });
+};
 
 //  @desc       get bootcamp
 //  @route      GET /api/v1/bootcamps:/id
 //  @access     Public
-exports.getBootcamp = (req,res,next)=> {
-    res.status(200).json({ success: true, msg: `get bootcamp ${req.params.id}` });
-}
+exports.getBootcamp = (req, res, next) => {
+  res.status(200).json({ success: true, msg: `get bootcamp ${req.params.id}` });
+};
 
 //  @desc       create new bootcamp
 //  @route      POST /api/v1/bootcamps
 //  @access     Private
-exports.createBootcamps = (req,res,next)=> {
-    res.status(200).json({ success: true, msg: 'create new bootcamp'});
-}
+exports.createBootcamps = (req, res, next) => {
+  res.status(200).json({ success: true, msg: 'create new bootcamp' });
+};
 
 //  @desc       Update bootcamp
 //  @route      PUT /api/v1/bootcamps:/id
 //  @access     Private
-exports.updateBootcamps = (req,res,next)=> {
-    res.status(200).json({ success: true, msg: `update bootcamp ${req.params.id}` });
-}
+exports.updateBootcamps = (req, res, next) => {
+  res
+    .status(200)
+    .json({ success: true, msg: `update bootcamp ${req.params.id}` });
+};
 
 //  @desc       delete bootcamp
 //  @route      DELETE /api/v1/bootcamps:/id
 //  @access     Private
-exports.deleteBootcamps = (req,res,next)=> {
-    res.status(200).json({ success: true, msg: `delete bootcamp ${req.params.id}` });
-}
+exports.deleteBootcamps = (req, res, next) => {
+  res
+    .status(200)
+    .json({ success: true, msg: `delete bootcamp ${req.params.id}` });
+};
--- backend/server.js
+++ backend/server.js
@@ -1,25 +1,34 @@
 const express = require('express');
-const dotenv =  require('dotenv');
-const morgan = require('morgan')
-
-// Route files
-const bootcamps = require('./routes/bootcamps');
+const dotenv = require('dotenv');
+const morgan = require('morgan');
+const connectDB = require('./config/db');
 
 //env vars
-dotenv.config({path:'./config/config.env'});
+dotenv.config({ path: './config/config.env' });
+
+connectDB();
+// Route files
+const bootcamps = require('./routes/bootcamps');
 
 const app = express();
 
 //middleware Dev logging
 
-if(process.env.NODE_ENV ==="development"){
-    app.use(morgan('dev'));
+if (process.env.NODE_ENV === 'development') {
+  app.use(morgan('dev'));
 }
-
 
 //mount routers
 app.use('/api/v1/bootcamps', bootcamps);
 
-const PORT =process.env.PORT || 5000;
+const PORT = process.env.PORT || 5000;
 
-app.listen(PORT,console.log(`server is running on port ${PORT}`));
+const server = app.listen(
+                            PORT,
+                            console.log(`server is running on port ${PORT}`));
+//handle unhandled promise rejections
+process.on('unhandledRejection',(err,promise)=>{
+    console.log(`Error: ${err.message}`);
+    //close server and exit
+    server.close(()=>process.exit(1));
+});
