93954fd18cbae2809b7c84cb9bd38038817daf8e
merttrkr <mertturker99@gmail.com>
2023-02-21 22:26:20 +0300

    added login method

--- backend/controllers/auth.js
+++ backend/controllers/auth.js
@@ -26,3 +26,40 @@ exports.register = asyncHandler(async (req, res, next) => {
   });
 
 });
+
+//  @desc       login user
+//  @route      POST /api/v1/auth/login
+//  @access     Public
+
+exports.login = asyncHandler(async (req, res, next) => {
+  const {email, password} = req.body;
+  
+  //  validate email and password
+  if (!email || !password) {
+    return next(new ErrorResponse(`please provide an email and password`,400));
+  }
+
+  // Check for user
+  const user = await User.findOne({ email }).select('+password');
+  
+  if (!user) {
+    return next(new ErrorResponse('Invalid credentials', 401));
+  }
+
+  // Check if password matches
+  const isMatch = await user.matchPassword(password);
+
+  if (!isMatch) {
+    return next(new ErrorResponse('Invalid credentials', 401));
+  }
+
+  //  Create token
+  const token =user.getSignedJwtToken();
+
+  res.status(200).json({
+    success: true,
+    token
+  });
+
+});
+
