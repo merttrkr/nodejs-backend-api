8797d1cbb93ea01274283023840c24ce82b51010
merttrkr <mertturker99@gmail.com>
2023-02-21 21:55:10 +0300

    added jsonwebtoken

--- backend/controllers/auth.js
+++ backend/controllers/auth.js
@@ -17,10 +17,12 @@ exports.register = asyncHandler(async (req, res, next) => {
     role
   });
 
-  
+  //  Create token
+  const token =user.getSignedJwtToken();
+
   res.status(200).json({
-    success: true
-    
+    success: true,
+    token
   });
 
 });
