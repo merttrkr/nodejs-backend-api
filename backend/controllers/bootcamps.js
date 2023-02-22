af8f63e2d0ad9a3f7911c13dcb90c37eeab1fcb4
merttrkr <mertturker99@gmail.com>
2023-02-22 14:48:31 +0300

    role chec for create bootcamp

--- backend/controllers/bootcamps.js
+++ backend/controllers/bootcamps.js
@@ -34,12 +34,25 @@ exports.getBootcamp = asyncHandler(async(req, res, next) => {
 //  @route      POST /api/v1/bootcamps
 //  @access     Private
 exports.createBootcamps = asyncHandler(async (req, res, next) => {
-  
-    const bootcamp = await Bootcamp.create(req.body);
-    res.status(201).json({
-      success: true,
-      data: bootcamp,
-    });
+  // add user to req body
+  req.body.user = req.user.id;
+
+  // check for published bootcamp
+  const publishedBootcamp = await Bootcamp.findOne({user: req.user.id});
+
+  //only one bootcamp can be added
+  if (publishedBootcamp && req.user.role !=='admin') {
+    return next(
+      new ErrorResponse(
+        `the user with ID ${req.user.id} has already published bootcamp`,400
+      )
+    );
+  }
+  const bootcamp = await Bootcamp.create(req.body);
+  res.status(201).json({
+    success: true,
+    data: bootcamp,
+  });
 
 });
 //  @desc       Update bootcamp
